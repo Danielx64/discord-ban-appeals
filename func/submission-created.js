451a5ef2505b52a7d490a78a7636b9e86e49482b
Danielx64 <289220+Danielx64@users.noreply.github.com>
2020-08-16 19:07:08 +1000

    updated forms details

--- func/submission-created.js
+++ func/submission-created.js
@@ -17,17 +17,18 @@ exports.handler = async function (event, context) {
 
         const params = new URLSearchParams(event.body);
         payload = {
-            banReason: params.get("banReason") || undefined,
-            appealText: params.get("appealText") || undefined,
-            futureActions: params.get("futureActions") || undefined,
+            discordtime: params.get("discordtime") || undefined,
+            timezone: params.get("timezone") || undefined,
+            why: params.get("why") || undefined,
+            aboutyourself: params.get("aboutyourself") || undefined,
+            moderatedbefore: params.get("moderatedbefore") || undefined,
+            currentmod: params.get("currentmod") || undefined,
+            currentmodinfo: params.get("currentmodinfo") || undefined,
             token: params.get("token") || undefined
         };
     }
 
-    if (payload.banReason !== undefined &&
-        payload.appealText !== undefined &&
-        payload.futureActions !== undefined && 
-        payload.token !== undefined) {
+    if (payload.token !== undefined) {
         
         const userInfo = decodeJwt(payload.token);
         const embedFields = [
@@ -36,19 +37,38 @@ exports.handler = async function (event, context) {
                 value: `<@${userInfo.id}> (${userInfo.username}#${userInfo.discriminator})`
             },
             {
-                name: "Why where you banned?",
-                value: payload.banReason
+                name: "How long have you been an active user of Discord?",
+                value: payload.discordtime
             },
             {
-                name: "Why do you feel you should be unbanned?",
-                value: payload.appealText
+                name: "Which timezone do you currently live in?",
+                value: payload.timezone
             },
             {
-                name: "What will you do to avoid being banned in the future?",
-                value: payload.futureActions
+                name: "Why do you want to be a moderator of this particular community?",
+                value: payload.why
+            },
+            {
+                name: "Something about yourself(optional)",
+                value: payload.aboutyourself
+            }
+            ,
+            {
+                name: "Have you ever moderated before on Discord?",
+                value: payload.moderatedbefore
+            }
+            ,
+            {
+                name: "Do you currently moderate any kind of community?",
+                value: payload.currentmod
+            }
+            ,
+            {
+                name: "What kind of community is it? What are you responsible for there?",
+                value: payload.currentmodinfo
             }
         ];
-        
+
         const result = await fetch(`${API_ENDPOINT}/channels/${encodeURIComponent(process.env.APPEALS_CHANNEL)}/messages`, {
             method: "POST",
             headers: {
